17430fd5c5bb7c9baa7053f2c7c4adc39f45ea6b
Gabriel D. <gabrielpanovich@gmail.com>
2025-12-07 00:53:54 -0300

    Criação de uma visualização das tasks escritas no arquivo de tasks e adição das flags no arquivo, que indicam se ela já foi concluída ou não.

--- main.asm
+++ main.asm
@@ -1,8 +1,11 @@
 .data
 instrucao_inicial:	.asciiz "------ Digite suas 4 tarefas ------\n"	
+texto_opcoes:	.asciiz "\nO que você deseja fazer?"
+opcoes_post:	.asciiz "\n0: próximo post\n1: comentários\n2: perfil do autor\n3: concluir tarefa\n"
 posts_filename:	.asciiz "posts.txt"
 tasks_filename:	.asciiz "tasks.txt"
-flag:	.word 0
+breakline:	.asciiz "\n"
+flag:	.byte 0
 
 .text
 .globl main
@@ -14,6 +17,39 @@ main:
 	la $a0, tasks_filename
 	jal write_tasks
 
+main_loop:
+	# LOOP QUE DESCREVE UMA ESCOLHA DE ATIVIDADE DO USUÁRIO (ir para um post, completar uma tarefa, etc)
+	li $v0, 4
+	la $a0, texto_opcoes
+	syscall
+	li $v0, 4
+	la $a0, opcoes_post
+	syscall
+	
+	li $v0, 5
+	syscall
+	sb $v0, flag
+	
+	# --------------------------------------------------
+	# VERIFICAÇÃO SE A FLAG FOI ARMAZENADA CORRETAMENTE
+	# li $v0, 1
+	# lb $a0, flag
+	# syscall
+	# --------------------------------------------------
+		
+	lb $t0, flag
+	beq $t0, 3, complete_task # precisa adicionar as outras flags antes dessa
+	
+	j main_loop
+	
+complete_task:
+	# Quebra de linha inicial
+	li $v0, 4
+	la $a0, breakline
+	syscall
+	la $a0, tasks_filename
+	jal show_tasks
+		
     	la $a0, posts_filename
     	jal load_posts
 
